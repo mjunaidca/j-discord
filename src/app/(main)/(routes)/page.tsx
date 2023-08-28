5ae083866320ec9978878a6e08d8f928e982dd00
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-29 03:03:38 +0500

    Inital Project Setup

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="fkex flex-col">
+      <p className="text-3xl font-bold text-indigo-400">
+        Protected Routes discord clone
+      </p>
+    </div>
+  );
+}
