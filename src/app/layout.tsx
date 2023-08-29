eac8f458fff8d428037453d5f6bafe7fac738ac3
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-30 01:21:39 +0500

    create modals provider and setup dynamic modal rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
-import "./globals.css";
-import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+
+import "./globals.css";
+
+import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/providers/next-provider";
 import { cn } from "@/lib/utils";
+
+import { ThemeProvider } from "@/components/providers/next-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -27,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
