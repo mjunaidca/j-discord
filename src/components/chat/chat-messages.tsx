4abae667000cccb8df31f08364ac5daffe0fe57e
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-08-31 16:45:26 +0500

    feat - create chat auto scroll hook

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -9,6 +9,7 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { ChatItem } from "./chat-item";
 import { useChatSocket } from "@/hooks/use-chat-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroo";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -58,6 +59,14 @@ export const ChatMessages = ({
 
   useChatSocket({ queryKey, addKey, updateKey });
 
+  useChatScroll({
+    chatRef,
+    bottomRef,
+    loadMore: fetchNextPage,
+    shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+    count: data?.pages?.[0]?.items?.length ?? 0,
+  });
+
   if (status === "loading") {
     return (
       <div className="flex flex-col flex-1 justify-center items-center">
@@ -82,8 +91,25 @@ export const ChatMessages = ({
 
   return (
     <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
-      <div className="flex-1" />
-      <ChatWelcome type={type} name={name} />
+      {!hasNextPage && <div className="flex-1" />}
+
+      {!hasNextPage && <ChatWelcome type={type} name={name} />}
+
+      {hasNextPage && (
+        <div className="flex justify-center">
+          {isFetchingNextPage ? (
+            <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchNextPage()}
+              className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"
+            >
+              Load previous messages
+            </button>
+          )}
+        </div>
+      )}
+
       <div className="flex flex-col mt-auto">
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
@@ -105,6 +131,7 @@ export const ChatMessages = ({
           </Fragment>
         ))}
       </div>
+      <div ref={bottomRef} />
     </div>
   );
 };
